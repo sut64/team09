7e7d5496e2eab9208d64496d435b062dfe0ead4a
Hitmepls <saranuykon@gmail.com>
2022-01-28 03:42:59 +0700

    backend

---
+++ backend/go.mod
@@ -0,0 +1,39 @@
+module github.com/tzcap/prescription
+
+go 1.17
+
+require (
+	github.com/gin-gonic/gin v1.7.7
+	gorm.io/driver/sqlite v1.2.6
+	gorm.io/gorm v1.22.4
+)
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/onsi/gomega v1.18.0 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-sqlite3 v1.14.10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/ugorji/go/codec v1.2.6 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ backend/main.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tzcap/prescription/controller"
+	"github.com/tzcap/prescription/entity"
+	"github.com/tzcap/prescription/middlewares"
+)
+
+func main() {
+	entity.SetupDatabase()
+
+	r := gin.Default()
+	r.Use(CORSMiddleware())
+	api := r.Group("")
+	{
+		protected := api.Use(middlewares.Authorizes())
+		{
+			// Authority Routes
+			protected.GET("/authorities", controller.ListAuthorities)
+			protected.GET("/authority/:id", controller.GetAuthority)
+			protected.POST("/authorities", controller.CreateAuthority)
+			protected.PATCH("/authorities", controller.UpdateAuthority)
+			protected.DELETE("/authorities/:id", controller.DeleteAuthority)
+
+			// Medicine Room Routes
+			protected.GET("/medicineRooms", controller.ListMedicineRooms)
+			protected.GET("/medicineRoom/:id", controller.GetMedicineRoom)
+			protected.POST("/medicineRooms", controller.CreateMedicineRoom)
+			protected.PATCH("/medicineRooms", controller.UpdateMedicineRoom)
+			protected.DELETE("/medicineRooms/:id", controller.DeleteMedicineRoom)
+
+			// Payment Status Routes
+			protected.GET("/paymentStatuses", controller.ListPaymentStatuses)
+			protected.GET("/paymentStatus/:id", controller.GetPaymentStatus)
+			protected.POST("/paymentStatuses", controller.CreatePaymentStatus)
+			protected.PATCH("/paymentStatuses", controller.UpdatePaymentStatus)
+			protected.DELETE("/paymentStatuses/:id", controller.DeletePaymentStatus)
+
+			// Prescription Routes
+			protected.GET("/Prescriptions", controller.ListPrescriptions)
+			protected.GET("/Prescription/:id", controller.GetPrescription)
+			protected.POST("/Prescriptions", controller.CreatePrescription)
+			protected.PATCH("/Prescriptions", controller.UpdatePrescription)
+			protected.DELETE("/Prescriptions/:id", controller.DeletePrescription)
+
+			// Suggestion Routes
+			protected.GET("/suggestions", controller.ListSuggestion)
+			protected.GET("/suggestion/:id", controller.GetSuggestion)
+			protected.POST("/suggestions", controller.CreateSuggestion)
+			protected.PATCH("/suggestions", controller.UpdateSuggestion)
+			protected.DELETE("/suggestions/:id", controller.DeleteSuggestion)
+
+			// Effect Routes
+			protected.GET("/effects", controller.ListEffect)
+			protected.GET("/effect/:id", controller.GetEffect)
+			protected.POST("/effects", controller.CreateEffect)
+			protected.PATCH("/effects", controller.UpdateEffect)
+			protected.DELETE("/effects/:id", controller.DeleteEffect)
+
+			//MedicineLabels Routes
+			protected.GET("/medicineLabels", controller.ListMedicineLabel)
+			protected.GET("/medicineLabel/:id", controller.GetMedicineLabel)
+			protected.POST("/medicineLabels", controller.CreateMedicineLabel)
+			protected.PATCH("/medicineLabels", controller.UpdateMedicineLabel)
+			protected.DELETE("/medicineLabels/:id", controller.DeleteMedicineLabel)
+
+			// MedicineStorage Routes
+			protected.GET("/medicineStorages", controller.ListMedicineStorage)
+			protected.GET("/medicineStorage/:id", controller.GetMedicineStorage)
+			protected.POST("/medicineStorages", controller.CreateMedicineStorage)
+			protected.PATCH("/medicineStorages", controller.UpdateMedicineStorage)
+			protected.DELETE("/medicineStorages/:id", controller.DeleteMedicineStorage)
+
+			// MedicineType Routes
+			protected.GET("/medicineTypes", controller.ListMedicineType)
+			protected.GET("/medicineType/:id", controller.GetMedicineType)
+			protected.POST("/medicineTypes", controller.CreateMedicineType)
+			protected.PATCH("/medicineTypes", controller.UpdateMedicineType)
+			protected.DELETE("/medicineTypes/:id", controller.DeleteMedicineType)
+
+			// MedicineDisbursement Routes
+			protected.GET("/disbursements", controller.ListMedicine_disbursement)
+			protected.GET("/disbursement/:id", controller.GetMedicine_disbursement)
+			protected.POST("/disbursements", controller.CreateMedicine_disbursement)
+			protected.PATCH("/disbursements", controller.UpdateMedicine_disbursement)
+			protected.DELETE("/disbursements/:id", controller.DeleteMedicine_disbursement)
+
+			//Packing
+			protected.GET("/packings", controller.ListPacking)
+			protected.GET("/packing/:id", controller.GetPacking)
+
+			//Medicinereceive
+			protected.POST("/medicinereceive", controller.CreateMedicinereceive)
+
+			// receive
+			protected.GET("/receives", controller.ListReceive)
+			protected.GET("/receive/:id", controller.GetReceive)
+
+			//DispenseMedicine Routes
+			protected.GET("/dispenseMedicines", controller.ListDispense_Medicine)
+			protected.GET("/dispenseMedicine/:id", controller.GetDispense_Medicine)
+			protected.POST("/dispenseMedicines", controller.CreateDispense_Medicine)
+			protected.PATCH("/dispenseMedicines", controller.UpdateDispense_Medicine)
+			protected.DELETE("/dispenseMediciness/:id", controller.DeleteDispense_Medicine)
+
+			//Paymentmethod Routes
+			protected.GET("/paymentmethods", controller.ListPaymentmethod)
+			protected.GET("/paymentmethod/:id", controller.GetPaymentmethod)
+			protected.POST("/paymentmethods", controller.CreatePaymentmethod)
+			protected.PATCH("/paymentmethods", controller.UpdatePaymentmethod)
+			protected.DELETE("/paymentmethods/:id", controller.DeletePaymentmethod)
+
+			//Price Routes
+			protected.GET("/prices", controller.ListPrice)
+			protected.GET("/price/:id", controller.GetPrice)
+			protected.POST("/prices", controller.CreatePrice)
+			protected.PATCH("/prices", controller.UpdatePrice)
+			protected.DELETE("/prices/:id", controller.DeletePrice)
+
+			//Bill Routes
+			protected.GET("/bills", controller.ListBill)
+			protected.GET("/bill/:id", controller.GetBill)
+			protected.POST("/bills", controller.CreateBill)
+			protected.PATCH("/bills", controller.UpdateBill)
+			protected.DELETE("/bills/:id", controller.DeleteBill)
+
+      // Dispense_status Routes
+			protected.GET("/dispense_statuses", controller.ListDispense_status)
+			protected.GET("/dispense_status/:id", controller.GetDispense_status)
+			protected.POST("/dispense_statuses", controller.CreateDispense_status)
+			protected.PATCH("/dispense_statuses", controller.UpdateDispense_status)
+			protected.DELETE("/dispense_statuses/:id", controller.DeleteDispense_status)
+		}
+	}
+	// Run the server
+	r.POST("/login", controller.Login)
+	r.Run()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
