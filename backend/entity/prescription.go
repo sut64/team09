98126619e300e588def5f279f143cf6d8515f7e8
Wanisa9701 <heonne94@gmail.com>
2022-02-02 15:52:28 +0700

    Update entity prescription

--- backend/entity/prescription.go
+++ backend/entity/prescription.go
@@ -9,17 +9,18 @@ import (
 
 type Prescription struct {
 	gorm.Model
-	PatientName        string `valid:"required~Name cannot be blank"`
-	PrescriptionNo     uint   `gorm:"uniqueIndex" valid:"range(100000|999999)~PrescriptionNo must be 6 digits"`
-	AuthoritiesID      *uint
-	Authorities        Authorities `gorm:"references:id" valid:"-"`
-	MedicineRoomID     *uint
-	MedicineRoom       MedicineRoom `gorm:"references:id" valid:"-"`
-	Amount             uint
-	PaymentStatusID    *uint
-	PaymentStatus      PaymentStatus      `gorm:"references:id" valid:"-"`
-	RecordingTime      time.Time          `valid:"notpast~RecordingTime not be past"`
+	PatientName      string `valid:"required~Name cannot be blank"`
+	PrescriptionNo   uint   `gorm:"uniqueIndex" valid:"range(100000|999999)~PrescriptionNo must be 6 digits"`
+	AuthoritiesID    *uint
+	Authorities      Authorities `gorm:"references:id"`
+	MedicineDisbursementID   *uint
+	MedicineDisbursement     MedicineDisbursement `gorm:"references:id"`
+	Amount           uint
+	Payment_statusID *uint
+	Payment_status   PaymentStatus `gorm:"references:id"`
+	RecordingTime    time.Time      `valid:"notpast~RecordingTime not be past"`
 	Dispense_Medicines []DispenseMedicine `gorm:"foreignKey:PrescriptionID"`
+
 }
 
 type PaymentStatus struct {
