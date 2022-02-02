f19589841d0f810e9a167dabd54ac250a0e54f60
yotsaphon08 <yotsaphon_ai@hotmail.com>
2022-02-03 04:20:45 +0700

    Update Validation Bill.go

--- backend/controller/Bill.go
+++ backend/controller/Bill.go
@@ -1,75 +1,78 @@
 package controller
 
 import (
-	"github.com/tzcap/prescription/entity"
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/gin-gonic/gin"
+	"github.com/tzcap/prescription/entity"
 )
 
 // POST /cases
 func CreateBill(c *gin.Context) {
 
 	var bills entity.Bill
-	var dispenseMedicines entity.DispenseMedicine
-	var prices entity.Price
+	var prescriptions entity.Prescription
 	var paymentmethods entity.Paymentmethod
 	var authority entity.Authorities
 
-	// ผลลัพธ์ที่ได้จากขั้นตอนที่ 9 จะถูก bind เข้าตัวแปร bill
+	// ผลลัพธ์ที่ได้จากขั้นตอนที่ 7 จะถูก bind เข้าตัวแปร bill
 	if err := c.ShouldBindJSON(&bills); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	// 10: ค้นหา characteristic ด้วย id
-	if tx := entity.DB().Where("id = ?", bills.DispenseMedicineID).First(&dispenseMedicines); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "dispense_medicines not found"})
+	// 8: ค้นหา Prescription ด้วย id
+	if tx := entity.DB().Where("id = ?", bills.PrescriptionID).First(&prescriptions); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "prescription not found"})
 		return
 	}
 
-	// 11: ค้นหา price ด้วย id
-	if tx := entity.DB().Where("id = ?", bills.PriceID).First(&prices); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "prices not found"})
-		return
-	}
-
-	// 12: ค้นหา paymentmethod ด้วย id
+	// 9: ค้นหา paymentmethod ด้วย id
 	if tx := entity.DB().Where("id = ?", bills.PaymentmethodID).First(&paymentmethods); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "paymentmethods not found"})
 		return
 	}
 
-	// 13: ค้นหา informer ด้วย id
+	// 10: ค้นหา informer ด้วย id
 	if tx := entity.DB().Where("id = ?", bills.AuthoritiesID).First(&authority); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "authorities not found"})
 		return
 	}
 
-	// 14: สร้าง Bill
+	// 11: สร้าง Bill
 	bi := entity.Bill{
-		DispenseMedicine: dispenseMedicines, // โยงความสัมพันธ์กับ Entity DispenseMedicine
-		Price:            prices,            // โยงความสัมพันธ์กับ Entity Price
-		Paymentmethod:    paymentmethods,    // โยงความสัมพันธ์กับ Entity Paymentmethod
-		Authorities:        authority,         // โยงความสัมพันธ์กับ Entity Authority
-		BillTime:         bills.BillTime,    // ตั้งค่าฟิลด์ BillTime
-		BillNo:           bills.BillNo,      // ตั้งค่าฟิลด์ BillNo
-		Payer:            bills.Payer,       // ตั้งค่าฟิลด์ Payer
-		Total:            bills.Total,       // ตั้งค่าฟิลด์ Total
+		Prescription:  prescriptions,  // โยงความสัมพันธ์กับ Entity Prescription
+		Paymentmethod: paymentmethods, // โยงความสัมพันธ์กับ Entity Paymentmethod
+		Authorities:   authority,      // โยงความสัมพันธ์กับ Entity Authority
+		BillTime:      bills.BillTime, // ตั้งค่าฟิลด์ BillTime
+		BillNo:        bills.BillNo,   // ตั้งค่าฟิลด์ BillNo
+		Payer:         bills.Payer,    // ตั้งค่าฟิลด์ Payer
+		Total:         bills.Total,    // ตั้งค่าฟิลด์ Total
 	}
 
-	// 15: บันทึก
+	// validate Bill controller
+	if _, err := govalidator.ValidateStruct(bi); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// 12: บันทึก
 	if err := entity.DB().Create(&bi).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"data": bi})
+
 }
 
 // GET /bill/:id
 func GetBill(c *gin.Context) {
 	var bills entity.Bill
 	id := c.Param("id")
-	if err := entity.DB().Preload("DispenseMedicine").Preload("Price").Preload("Paymentmethod").Preload("Authorities").Raw("SELECT * FROM bills WHERE id = ?", id).Find(&bills).Error; err != nil {
+	if err := entity.DB().Preload("Prescription").Preload("Prescription.MedicineDisbursement").
+		Preload("Prescription.MedicineDisbursement.MedicineStorage").Preload("Paymentmethod").
+		Preload("Authorities").Raw("SELECT * FROM bills WHERE id = ?", id).Find(&bills).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
@@ -79,7 +82,9 @@ func GetBill(c *gin.Context) {
 // GET /bills
 func ListBill(c *gin.Context) {
 	var bills []entity.Bill
-	if err := entity.DB().Preload("DispenseMedicine").Preload("Price").Preload("Paymentmethod").Preload("Authorities").Raw("SELECT * FROM bills").Find(&bills).Error; err != nil {
+	if err := entity.DB().Preload("Prescription").Preload("Prescription.MedicineDisbursement").
+		Preload("Prescription.MedicineDisbursement.MedicineStorage").Preload("Paymentmethod").
+		Preload("Authorities").Raw("SELECT * FROM bills").Find(&bills).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
